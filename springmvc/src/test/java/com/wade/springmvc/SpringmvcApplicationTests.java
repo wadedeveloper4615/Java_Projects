d73526ebf2c1e0e290f2d4328d776d207e360f34
WADEOS <wade4615@gmail.com>
2021-01-18 12:45:45 -0600

    initial commit

---
+++ springmvc/src/test/java/com/wade/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,12 @@
+package com.wade.springmvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}

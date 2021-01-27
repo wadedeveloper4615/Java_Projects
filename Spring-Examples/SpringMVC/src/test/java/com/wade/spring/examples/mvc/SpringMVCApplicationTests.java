7de8001a6b6b5f1495a55b3a2910daf38ad84765
WADEOS <wade4615@gmail.com>
2021-01-27 09:13:37 -0600

    added home controller to springmvc

--- Spring-Examples/SpringMVC/src/test/java/com/wade/spring/examples/mvc/SpringMVCApplicationTests.java
+++ Spring-Examples/SpringMVC/src/test/java/com/wade/spring/examples/mvc/SpringMVCApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MvcApplicationTests {
+class SpringMVCApplicationTests {
 
 	@Test
 	void contextLoads() {

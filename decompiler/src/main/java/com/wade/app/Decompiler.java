c5a335bd162b2a13043eb786e0afcadef7a68a29
WADEOS <wade4615@gmail.com>
2021-01-30 17:11:03 -0600

    initial check for decompiler

---
+++ decompiler/src/main/java/com/wade/app/Decompiler.java
@@ -0,0 +1,18 @@
+package com.wade.app;
+
+import org.apache.bcel.classfile.ClassParser;
+import org.apache.bcel.classfile.JavaClass;
+
+public class Decompiler {
+	  public static void main(String[] argv) {
+		try {
+			for (int i = 0; i < argv.length; i++) {
+				if (argv[i].endsWith(".class")) {
+					JavaClass java_class = new ClassParser(argv[i]).parse();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ decompiler/src/test/java/com/wade/app/DecompilerTest.java
@@ -0,0 +1,10 @@
+package com.wade.app;
+
+import org.junit.jupiter.api.Test;
+
+class DecompilerTest {
+	@Test
+	void test() {
+	}
+
+}
